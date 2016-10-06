d474f6b58b49b04f051ad1e369de6dc666653b55
adorris1 <ashleydorris89@gmail.com>
2016-10-06 08:42:27 -0700

    working except for sass stuff

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311114503) do
-
-  create_table "test_models", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20140303080934) do
 
   create_table "users", force: true do |t|
     t.string   "email",                        default: "", null: false
